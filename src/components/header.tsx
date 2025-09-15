b97c9ff51c6f1c234b6024930b31e2b065f4cd67
DamienWatson <damiensinbox@gmail.com>
2025-09-15 12:45:44 +1000

    Switch icon on details

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import Icon from './icon';
+import { Link, useParams } from 'react-router-dom';
 import classes from './header.module.scss';
 import OptionsIcon from './icons/options-icon';
 import UserIcon from './icons/user-icon';
 import EatClubIcon from './icons/eatclub-icon';
+import BackIcon from './icons/back-icon';
 
-const Header = () => (
-  <header className={classes.header}>
-    <Icon>
+const Header = () => {
+  const params = useParams();
+
+  return (
+    <header className={classes.header}>
       <UserIcon />
-    </Icon>
-    
-    <Link to="/">
-      <Icon>
-        <EatClubIcon colour="#ce4837" />
-      </Icon>
-    </Link>
-    
-    <Icon>
+      
+      <Link to="/">
+        {params.id === undefined
+          ? <EatClubIcon colour="#ce4837" />
+          : <BackIcon />}
+      </Link>
+      
       <OptionsIcon />
-    </Icon>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
--- src/components/icons/back-icon.tsx
+++ src/components/icons/back-icon.tsx
@@ -4,7 +4,7 @@ import { iconProps } from '../../types/icons';
 const BackIcon = ({ size="24", colour="currentColor"}: iconProps) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke={colour} className="size-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="m11.5 9.5-3 3m0 0 3 3m-3-3h7.5M23 12a11 11 0 1 1-22 0 11 11 0 0 1 22 0Z" />
     </svg>
   );
 };
