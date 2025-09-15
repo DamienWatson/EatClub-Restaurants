a5bf650669e10177199b9e9514436fd4207e9d8a
DamienWatson <damiensinbox@gmail.com>
2025-09-15 15:16:03 +1000

    Add desktop view for Restaurant details

--- src/components/details-content.tsx
+++ src/components/details-content.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { RestaurantType } from '../types/restaurants';
-import classes from './details-content.module.scss';
 import ClockIcon from './icons/clock-icon';
 import LocationIcon from './icons/location-icon';
 import DealList from './deal-list';
+import classes from './details-content.module.scss';
 
 type DetailsContentProps = {
   restaurant: RestaurantType,
@@ -28,17 +28,19 @@ const DetailsContent = ({ restaurant }: DetailsContentProps) => {
 
   return (
     <div className={classes.container}>
-      <h1 className={classes.heading}>{name}</h1>
-      <p className={classes.tagline}>{tagList.join(' • ')}</p>
+      <div className={classes.screenContent}>
+        <h1 className={classes.heading}>{name}</h1>
+        <p className={classes.tagline}>{tagList.join(' • ')}</p>
 
-      <div className={classes.infoList}>
-        <ClockIcon size="32" />
-        <p>Hours: {open} - {close}</p>
-        <LocationIcon size="32" />
-        <p>{address1}, {suburb}</p>
+        <div className={classes.infoList}>
+          <ClockIcon size="32" />
+          <p>Hours: {open} - {close}</p>
+          <LocationIcon size="32" />
+          <p>{address1}, {suburb}</p>
+        </div>
+
+        <DealList deals={deals} hours={hours} />
       </div>
-
-      <DealList deals={deals} hours={hours} />
     </div>
   );
 };
--- src/components/details-menu.tsx
+++ src/components/details-menu.tsx
@@ -1,42 +1,43 @@
 import React from 'react';
-import Icon from './icon';
 import MenuIcon from './icons/menu-icon';
 import ContactIcon from './icons/contact-icon';
 import LocationMapIcon from './icons/location-map-icon';
 import HeartIcon from './icons/heart-icon';
-import classes from './details-menu.module.scss';
 import IconButton from './icon-button';
+import classes from './details-menu.module.scss';
 
 const DetailsMenu = () => {
   return (
     <div className={classes.container}>
-      <IconButton
-        label="Menu"
-        width="xxl"
-      >
-        <MenuIcon size="32" />
-      </IconButton>
+      <div className={classes.screenContent}>
+        <IconButton
+          label="Menu"
+          width="xxl"
+        >
+          <MenuIcon size="32" />
+        </IconButton>
 
-      <IconButton
-        label="Call us"
-        width="xxl"
-      >
-        <ContactIcon size="32" />
-      </IconButton>
+        <IconButton
+          label="Call us"
+          width="xxl"
+        >
+          <ContactIcon size="32" />
+        </IconButton>
 
-      <IconButton
-        label="Location"
-        width="xxl"
-      >
-        <LocationMapIcon size="32" />
-      </IconButton>
+        <IconButton
+          label="Location"
+          width="xxl"
+        >
+          <LocationMapIcon size="32" />
+        </IconButton>
 
-      <IconButton
-        label="Favourite"
-        width="xxl"
-      >
-        <HeartIcon size="32" />
-      </IconButton>
+        <IconButton
+          label="Favourite"
+          width="xxl"
+        >
+          <HeartIcon size="32" />
+        </IconButton>
+      </div>
     </div>
   );
 };
